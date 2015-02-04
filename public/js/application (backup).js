6afeae767caf5e785f8f0d635186e155a7c71b57
amirulabu <amirulabu@gmail.com>
2015-02-04 15:31:48 +0800

    tried some oop

---
+++ public/js/application (backup).js
@@ -0,0 +1,68 @@
+$(document).ready(function(){
+$('#hidden').hide();
+   // jQuery methods go here...
+
+
+   var p = 0;
+   var q = 0;
+  $( "body" ).keyup(function( event ) {
+      if(event.which == 80) {
+        p += 1;
+     $( "tr#player1_strip td" ).removeClass('active');
+     $( "tr#player1_strip td:nth-child("+ p +")" ).addClass('active');
+      }
+      if(p == block){
+        atr = $('#player1').attr('name');
+        time = $('#time').attr('name');
+        alert(""+ atr + " win")
+        console.log(atr);
+        //$('#win_input').val(atr)
+        // $('#target').submit();
+        var request = $.ajax({
+          type: "POST",
+          url: "/game",
+          dataType: "json",
+          data: {"win_input": atr,"timestart":time}
+        })
+        request.done(function(msg){
+          window.location.href = '/';
+        });
+
+      }
+  });
+
+   $( "body" ).keyup(function( event ) {
+      if(event.which == 81) {
+        q += 1;
+     $( "tr#player2_strip td" ).removeClass('active');
+     $( "tr#player2_strip td:nth-child("+ q +")" ).addClass('active');
+      }
+
+
+        if(q == block){
+        atr = $('#player2').attr('name');
+        time = $('#time').attr('name');
+        alert(""+ atr + " win")
+
+        console.log(atr);
+        //$('#win_input').val(atr)
+        // $('#target').submit();
+        var request = $.ajax({
+          type: "POST",
+          url: "/game",
+          dataType: "json",
+          data: {"win_input": atr, "timestart": time}
+        })
+        request.done(function(msg){
+          window.location.href = '/';
+        });
+
+      }
+  });
+
+// end
+});
+
+
+
+
--- public/js/application.js
+++ public/js/application.js
@@ -3,63 +3,63 @@ $('#hidden').hide();
    // jQuery methods go here...
 
 
-   var p = 0;
-   var q = 0;
-  $( "body" ).keyup(function( event ) {
-      if(event.which == 80) {
-        p += 1;
-     $( "tr#player1_strip td" ).removeClass('active');
-     $( "tr#player1_strip td:nth-child("+ p +")" ).addClass('active');
-      }
-      if(p == block){
-        atr = $('#player1').attr('name');
-        time = $('#time').attr('name');
-        alert(""+ atr + " win")
-        console.log(atr);
-        //$('#win_input').val(atr)
-        // $('#target').submit();
-        var request = $.ajax({
-          type: "POST",
-          url: "/game",
-          dataType: "json",
-          data: {"win_input": atr,"timestart":time}
-        })
-        request.done(function(msg){
-          window.location.href = '/';
-        });
-
-      }
-  });
-
-   $( "body" ).keyup(function( event ) {
-      if(event.which == 81) {
-        q += 1;
-     $( "tr#player2_strip td" ).removeClass('active');
-     $( "tr#player2_strip td:nth-child("+ q +")" ).addClass('active');
-      }
-
-
+  function Game() {
+    //player 1
+    var p = 0;
+    $( "body" ).keyup(function( event ) {
+        if(event.which == 80) {
+          p += 1;
+       $( "tr#player1_strip td" ).removeClass('active');
+       $( "tr#player1_strip td:nth-child("+ p +")" ).addClass('active');
+        }
+        if(p == block){
+          atr = $('#player1').attr('name');
+          timetaken = $('#time').attr('name');
+          Winner(atr,timetaken);
+          //***************************************
+          //***************************************
+        }
+    });
+    //player 2
+     var q = 0;
+     $( "body" ).keyup(function( event ) {
+        if(event.which == 81) {
+          q += 1;
+       $( "tr#player2_strip td" ).removeClass('active');
+       $( "tr#player2_strip td:nth-child("+ q +")" ).addClass('active');
+        }
         if(q == block){
-        atr = $('#player2').attr('name');
-        time = $('#time').attr('name');
-        alert(""+ atr + " win")
+          atr = $('#player2').attr('name');
+          timetaken = $('#time').attr('name');
+          Winner(atr,timetaken);
+          //***************************************
+          //***************************************
+        }
+    });
 
-        console.log(atr);
-        //$('#win_input').val(atr)
-        // $('#target').submit();
-        var request = $.ajax({
-          type: "POST",
-          url: "/game",
-          dataType: "json",
-          data: {"win_input": atr, "timestart": time}
-        })
-        request.done(function(msg){
-          window.location.href = '/';
-        });
 
-      }
-  });
+    Winner = function winner1 (atr,timetaken) {
+    this.atr = atr;
+    this.timetaken = timetaken
+    alert(""+ atr + " win")
+    console.log(atr);
+    var request = $.ajax({
+      type: "POST",
+      url: "/game",
+      dataType: "json",
+      data: {"win_input": atr,"timestart":timetaken}
+    })
+    request.done(function(msg){
+      window.location.href = '/';
+    });
+  };
+  } //end of Game
 
+
+
+
+  var gamestart = new Game();
+  gamestart;
 // end
 });
 
